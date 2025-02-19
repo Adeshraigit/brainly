e04ff9fb899321fd8e6a178857221e9669bf4b15
Adesh <121770101+Adeshraigit@users.noreply.github.com>
2025-02-19 20:46:42 +0530

    fixing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
 import Dashboard from "./pages/dashboard"
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+import { BrowserRouter, Routes, Route } from "react-router-dom";    
 
 function App() {
-  return <Signin/>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
